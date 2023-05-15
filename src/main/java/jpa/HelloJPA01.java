88bd740c8b886cc736bd44ab842c6a743cd5e271
kdh6194 <kdh6194@naver.com>
2023-05-15 11:41:47 +0900

    jpa 설정 파일

--- src/main/java/jpa/HelloJPA01.java
+++ src/main/java/jpa/HelloJPA01.java
@@ -1,7 +1,25 @@
 package jpa;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
 public class HelloJPA01 {
     public static void main(String[] args) {
+    // jpa 객체 초기화 : emf -> em -> tx
+        // 보통 에러가 발생하는 이유는 persistence.xml에서 잘못 설정해서
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
 
+        try {
+
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            em.close();
+            emf.close();
+        }
     }
 }
