e3a36c72d04cc09ed51e110d2a0dae114cfd2d35
kdh6194 <kdh6194@naver.com>
2023-05-17 15:22:02 +0900

    querydsl 생성, 수정, 삭제

---
+++ src/main/java/dsl/HelloQueryDSL03.java
@@ -0,0 +1,76 @@
+package dsl;
+
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import model.Employee;
+import model.QDepartment;
+import model.QEmployee;
+import org.apache.commons.lang.StringUtils;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.Arrays;
+import java.util.List;
+
+public class HelloQueryDSL03 {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("default");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+
+        try{
+           //CRUD 활용 -> Transaction 기반으로 작성되어야한다.
+           // 사원 추가
+//            tx.begin();
+    //           Employee e = new Employee();
+    //            e.setFname("jobs");
+    //            e.setSal(15000L);
+    //           em.persist(e);
+//            tx.commit();
+
+            // 조회는 HelloQueryDSL02에 작성되어 있으니 참고하면 됨
+
+            // 사원 수정
+            QEmployee qemp = QEmployee.employee;
+            JPAQueryFactory query = new JPAQueryFactory(em);
+
+//            tx.begin();
+//                query.update(qemp)
+//                .set(qemp.fname,"abc123")
+//                .set(qemp.lname,"987xyz")
+//                .set(qemp.sal,15500L)
+//                .set(qemp.jobid,"Game_QC")
+//                .where(qemp.empno.eq(998L))
+//                .execute();
+//            tx.commit();
+
+            // 사원데이터 삭제
+
+//            tx.begin();
+//            query.delete(qemp)
+//                    .where(qemp.empno.eq(994L))
+//                    .execute();
+//            tx.commit();
+
+            // 사원데이터 삭제 - 한번에 삭제
+//            List<Long> empnoList = Arrays.asList(995L, 996L, 998L);
+//
+//            tx.begin();
+//            query.delete(qemp)
+//                    .where(qemp.empno.in(empnoList))
+//                    .execute();
+//            tx.commit();
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+            tx.rollback();
+        } finally {
+            em.close();
+            emf.close();
+        }
+    }
+}
